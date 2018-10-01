9dc011660c3444571d87948b4ceb0ec1b81462ab
Thekolllol90 <neosvarre@hotmail.se>
2018-10-01 10:56:03 +0200

    boi

--- src/loopar/tar_ett_tal_och_skriver_fakultet.java
+++ src/loopar/tar_ett_tal_och_skriver_fakultet.java
@@ -2,6 +2,18 @@ package loopar;
 import java.util.Scanner;
 public class tar_ett_tal_och_skriver_fakultet {
 	public static void main(String[] args) {
+		Scanner boi = new Scanner (System.in);
+		boolean a = true;
+		while(a) {
+		System.out.println("säg ett tal");
+		int x = boi.nextInt();
+		int res = 1;
+		for (int i = 1; i<=x; i++) {
+			res *= i;  
+		}
 		
+		System.out.println(res);
+		}
+		boi.close();
 	}
 }
--- src/loopar/x_xx_xxx_xxxx.java
+++ src/loopar/x_xx_xxx_xxxx.java
@@ -2,6 +2,11 @@ package loopar;
 
 public class x_xx_xxx_xxxx {
 	public static void main(String[] args) {
-		
+		for (int i = 0; i <= 10; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("x");
+			}
+			System.out.println();
+		}  
 	}
 }
